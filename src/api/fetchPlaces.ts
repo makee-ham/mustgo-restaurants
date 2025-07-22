d31c7ba6a714d7f56b9f2957ffc42781ee393bcd
Makee <ina0312@naver.com>
2025-07-22 15:15:42 +0900

    feat #5 fetchPlaces 로직에서 404 status 캐치

--- src/api/fetchPlaces.ts
+++ src/api/fetchPlaces.ts
@@ -3,7 +3,10 @@ const baseUrl = import.meta.env.VITE_API_BASE_URL;
 export async function fetchPlaces() {
   const response = await fetch(baseUrl + "places");
   if (!response.ok) {
-    throw new Error("맛집 데이터를 불러오지 못했습니다.");
+    if (response.status === 404) {
+      throw new Error("404");
+    }
+    throw new Error("unknown");
   }
   return response.json();
 }
