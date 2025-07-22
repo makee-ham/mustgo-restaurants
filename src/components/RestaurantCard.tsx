979c0ef947b8520239834091a6cfe0e19e7afc4c
Makee <ina0312@naver.com>
2025-07-22 14:02:30 +0900

    feat #3 카드 컴포넌트에 props로 전달받은 데이터 렌더링

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -1,16 +1,25 @@
-export default function RestaurantCard() {
+import type { Place } from "../types/place";
+
+type RestaurantCardProps = {
+  place: Place;
+};
+
+export default function RestaurantCard({ place }: RestaurantCardProps) {
+  const imageUrl = `${import.meta.env.VITE_API_BASE_URL}${place.image.src}`;
+
   return (
     <div className="relative rounded-xl overflow-hidden shadow-md cursor-pointer group">
       <img
-        src="https://placehold.co/600x400"
-        alt="맛집"
+        src={imageUrl}
+        alt={place.image.alt}
         className="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-105"
       />
 
       <div className="absolute bottom-0 w-full bg-base-100/70 text-base-content text-center py-1 text-sm font-medium">
-        맛집 이름
+        {place.title}
       </div>
 
+      {/* 찜하기 토글 버튼은 추후 찜 기능 때 디자인 할 것 */}
       <button
         className="absolute top-2 right-2 btn btn-sm btn-circle bg-base-100/80 text-base-content hover:bg-base-200"
         aria-label="찜하기"
