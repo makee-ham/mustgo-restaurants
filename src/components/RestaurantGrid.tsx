978ae7ae726f3acb6e41500c434c01d423845fef
Makee <ina0312@naver.com>
2025-07-22 13:52:32 +0900

    feat #3 그리드 컴포넌트에 상태와 페치 적용

--- src/components/RestaurantGrid.tsx
+++ src/components/RestaurantGrid.tsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
+import { fetchPlaces } from "../api/fetchPlaces";
 
 export default function RestaurantGrid() {
+  const [places, setPlaces] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchPlaces()
+      .then((data) => setPlaces(data.places))
+      .catch(console.error)
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       <RestaurantCard />
