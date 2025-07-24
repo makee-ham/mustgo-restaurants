c0e37375ffdad892c3447e40fa672e854a3a4f94
Makee <ina0312@naver.com>
2025-07-24 10:43:12 +0900

    feat #20 ErrorAlert 컴포넌트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Section from "./components/Section";
 import { Toaster } from "react-hot-toast";
 import DeleteModal from "./components/DeleteModal";
 import usePlaces from "./hooks/usePlaces";
+import ErrorAlert from "./components/ErrorAlert";
 
 export default function App() {
   const {
@@ -37,22 +38,7 @@ export default function App() {
       />
       <Page>
         {error ? (
-          <div className="alert alert-error shadow-lg mb-6">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="stroke-current shrink-0 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M18.364 5.636l-12.728 12.728m0-12.728l12.728 12.728"
-              />
-            </svg>
-            <span>{error}</span>
-          </div>
+          <ErrorAlert message={error} />
         ) : (
           <>
             <Section title="찜 목록">
